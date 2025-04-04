13f02961cd685ecb5d8ef4629917dd23651f41be
Ravichandra KR <ravichandra.kr@aylanetworks.com>
2025-04-04 10:05:56 +0530

    [New] SPM support

--- Package.swift
+++ Package.swift
@@ -1,26 +1,38 @@
-// swift-tools-version:5.0
+
+// swift-tools-version:5.9
+
+
 import PackageDescription
 
 let package = Package(
     name: "SocketRocket",
     platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v9),
+        .iOS(.v12),
+        .macOS(.v10_13),
+        .tvOS(.v12),
     ],
     products: [
         .library(
             name: "SocketRocket",
             type: .dynamic,
-            targets: ["SocketRocket"]),
+            targets: ["SocketRocketTarget"]),
     ],
     dependencies: [],
     targets: [
         .target(
-            name: "SocketRocket",
+            name: "SocketRocketTarget",
             path: "SocketRocket",
+            publicHeadersPath: "",
             cSettings: [
-                .headerSearchPath("./**"),
-            ]),
+                .headerSearchPath("."),
+                .headerSearchPath("Internal"),
+                .headerSearchPath("Internal/Delegate"),
+                .headerSearchPath("Internal/IOConsumer"),
+                .headerSearchPath("Internal/Proxy"),
+                .headerSearchPath("Internal/RunLoop"),
+                .headerSearchPath("Internal/Security"),
+                .headerSearchPath("Internal/Utilities")
+            ]
+        )
     ]
 )
